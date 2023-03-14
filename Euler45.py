55d8fb7908e2b0e4b2850934de9100fe4e620816
Abbie <ac2061@cantab.ac.uk>
2023-03-14 21:02:34 +0000

    forgot to check the roots not just the discriminant oops

--- Euler45.py
+++ Euler45.py
@@ -10,7 +10,7 @@
 
 def main():
     
-    n = 286
+    n = 500
     while True:
         triangle_number = find_next_triangle_number(n)
         if is_pentagonal(triangle_number) and is_hexagonal(triangle_number):
@@ -26,29 +26,36 @@ def is_pentagonal(triangle_number: int) -> bool:
     # 3n^2 - n - 2t = 0
     # don't need to solve for n, just need discriminant to be >=0 and square
     # D = b^2 - 4ac.
-
-    D: int = 1 + 4*3*2*triangle_number
-    print(D)
-    if D < 0:
-        return False
-    return is_square(D)
+    return real_integer_roots(a=3, b=-1, c=-2*triangle_number)
+    
 
 def is_hexagonal(triangle_number: int) -> bool:
     # Hn=n(2n−1)	
     # 2n^2 - n - t = 0
-    
-    D: int = 1 + 4*2*triangle_number
-    if D < 0:
-        return False
-    return is_square(D)
+    return real_integer_roots(a=2, b=-1, c=-triangle_number)
+
 
 def is_square(D: int) -> bool:
     # this relies on rounding, so not ideal solution
     root = D**0.5
-    print(root)
     if int(root)**2 == D:
         return True
     return False
+
+def real_integer_roots(a: int, b: int, c: int) -> bool:
+
+    D:int = b**2 - 4*a*c
+    if D < 0:
+        return False
+    elif not is_square(D):
+        return False
+
+    # b always == -1 and |b| < |D**0.5|, a always +ve => always want +ve D**0.5 value
+    x = (-b + D**0.5) / (2*a)
+    if int(x) == x:
+        return True
+    return False
+
 
 if __name__ == '__main__':
     main()
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,5 +1,5 @@
 import unittest
-from ..Euler45 import is_square, is_hexagonal, is_pentagonal
+from ..Euler45 import is_square, is_hexagonal, is_pentagonal, real_integer_roots
 
 
 def test_squares():
@@ -13,14 +13,19 @@ def test_non_squares():
     assert is_square(32432) == False
     assert is_square(40753) == False
 
-def test_pentagonal():
-    assert is_pentagonal(40755) == True
+# def test_pentagonal():
+#     assert is_pentagonal(40755) == True
 
-def test_hexagonal():
-    assert is_hexagonal(40755) == True
+# def test_hexagonal():
+#     assert is_hexagonal(40755) == True
 
-def test_non_pentagonal():
-    assert is_pentagonal(567645) == False
+# def test_non_pentagonal():
+#     assert is_pentagonal(567645) == False
 
-def test_non_hexagonal():
-    assert is_hexagonal(567645) == False
+# def test_non_hexagonal():
+#     assert is_hexagonal(567645) == False
+
+def test_root_integer():
+    assert real_integer_roots(3, -1, -2*40755) == True
+    assert real_integer_roots(3, -1, -2*40751) == False
+    assert real_integer_roots(2, -1, -40755) == True
